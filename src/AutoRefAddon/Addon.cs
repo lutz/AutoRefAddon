5663ddf2685ee18963edcd482df456fc3d16e11f
Daniel Lutz <daniel.lutz@outlook.com>
2019-12-27 13:07:49 +0100

    init code

---
+++ src/AutoRefAddon/Addon.cs
@@ -0,0 +1,110 @@
+﻿using Infragistics.Win.UltraWinToolbars;
+using SwissAcademic.Citavi.Shell;
+using SwissAcademic.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AutoRef
+{
+    public class Addon : CitaviAddOn
+    {
+        #region Fields
+
+        IEnumerable<string> _defaultAssemblies;
+
+        #endregion
+
+        #region Constructors
+
+        public Addon() => Application.OpenForms.AddListChangedEventHandler(Forms_Added, ListChangedType.Added);
+
+        #endregion
+
+        #region Properties
+        public override AddOnHostingForm HostingForm => AddOnHostingForm.None;
+
+        #endregion
+
+        #region Methods
+
+        void ChangedToolClickHandler(MacroEditorForm form)
+        {
+            var toolbarsManager = form.GetToolbarsManager();
+            var registredDelegates = toolbarsManager?.RemoveEventHandlersFromEvent("ToolClick");
+            ToolClickEventHandler clickEventHandler = (sender, args) =>
+            {
+                var e = new BeforePerformingCommandEventArgs(form, args.Tool.Key, args.ListToolItem?.Key, null);
+                OnBeforePerformingCommand(form, e);
+                if (!e.Handled)
+                {
+                    foreach (var del in registredDelegates)
+                    {
+                        del.DynamicInvoke(sender, args);
+                    }
+
+                    OnAfterPerformingCommand(form, e);
+                }
+            };
+
+            toolbarsManager?.AddEventHandlerForEvent("ToolClick", clickEventHandler);
+        }
+
+        void OnBeforePerformingCommand(MacroEditorForm macroEditorForm, BeforePerformingCommandEventArgs e)
+        {
+            if (e.Key.Equals("Save", StringComparison.OrdinalIgnoreCase))
+            {
+                var codeLines = macroEditorForm.GetReferencedAssemblies()
+                                                       .Where(assembly => _defaultAssemblies.FirstOrDefault(a => a.Equals(assembly, StringComparison.OrdinalIgnoreCase)) == null)
+                                                       .Select(ase => $"// #include \"{ase}\"\r")
+                                                       .ToList();
+
+                codeLines.AddRange(macroEditorForm.MacroCode.Split(new char[] { '\n' }).ToList());
+                macroEditorForm.MacroCode = string.Join("\n", codeLines);
+            }
+        }
+        
+        void OnAfterPerformingCommand(MacroEditorForm macroEditorForm, BeforePerformingCommandEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case "Save":
+                    {
+                        macroEditorForm.RemoveIncludes();
+                    }
+                    break;
+                case "Open":
+                    {
+                        var includes = macroEditorForm.ParseIncludes();
+                        macroEditorForm.ReferenceIncludes(includes);
+                        macroEditorForm.RemoveIncludes();
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+
+        #endregion
+
+        #region EventHandler
+
+        void Forms_Added(object sender, ListChangedEventArgs args)
+        {
+            foreach (var form in args.Forms)
+            {
+                if (form is MacroEditorForm macroEditorForm)
+                {
+                    ChangedToolClickHandler(macroEditorForm);
+
+                    if (_defaultAssemblies == null) _defaultAssemblies = macroEditorForm.GetReferencedAssemblies();
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/AutoRefAddon/Core/Extensions.cs
@@ -0,0 +1,88 @@
+﻿using SwissAcademic.Citavi.Shell;
+using SwissAcademic.Controls;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AutoRef
+{
+    internal static class Extensions
+    {
+        #region Fields
+
+        static readonly BindingFlags fieldBindingFlags = BindingFlags.Instance | BindingFlags.NonPublic;
+        static readonly BindingFlags staticEventBindingFlags = fieldBindingFlags | BindingFlags.Static;
+        static readonly BindingFlags staticFieldBindingFlags = BindingFlags.NonPublic | BindingFlags.Static;
+
+        #endregion
+
+        #region Methods
+
+        public static ToolbarsManager GetToolbarsManager<T>(this T form) where T : FormBase => form.GetType().GetField("toolbarsManager", fieldBindingFlags)?.GetValue(form) as ToolbarsManager;
+
+        public static IReadOnlyList<Delegate> RemoveEventHandlersFromEvent(this ToolbarsManager toolbarsManager, string eventName)
+        {
+
+            var eventsPropertyInfo = toolbarsManager
+                                    .GetType()
+                                    .GetProperties(staticEventBindingFlags)
+                                    .Where(p => p.Name.Equals("Events", StringComparison.OrdinalIgnoreCase) && p.PropertyType.Equals(typeof(Infragistics.Shared.EventHandlerDictionary)))
+                                    .FirstOrDefault();
+
+            var eventHandlerList = eventsPropertyInfo?
+                                   .GetValue(toolbarsManager, new object[] { }) as Infragistics.Shared.EventHandlerDictionary;
+
+            var eventFieldInfo = typeof(ToolbarsManager)
+                                  .BaseType
+                                  .GetFields(staticFieldBindingFlags)
+                                  .FirstOrDefault(fi => fi.Name.Equals("Event_" + eventName, StringComparison.OrdinalIgnoreCase));
+
+            var eventKey = eventFieldInfo.GetValue(toolbarsManager);
+
+            var currentEventHandler = eventHandlerList[eventKey] as Delegate;
+            Delegate[] currentRegistredHandlers = currentEventHandler.GetInvocationList();
+            foreach (var item in currentRegistredHandlers)
+            {
+                toolbarsManager.GetType().GetEvent(eventName).RemoveEventHandler(toolbarsManager, item);
+            }
+
+            return currentRegistredHandlers.ToList().AsReadOnly();
+        }
+
+        public static void AddEventHandlerForEvent(this ToolbarsManager toolbarsManager, string eventName, Delegate @delegate) => toolbarsManager.GetType().GetEvent(eventName).AddEventHandler(toolbarsManager, @delegate);
+
+
+        public static IEnumerable<string> GetReferencedAssemblies(this MacroEditorForm macroEditorForm)
+        {
+            var macroEditor = macroEditorForm.GetType().GetField("macroEditor", fieldBindingFlags)?.GetValue(macroEditorForm);
+            var compilerParameters = macroEditor?.GetType().GetField("_compilerParameters", fieldBindingFlags)?.GetValue(macroEditor) as CompilerParameters;
+            return compilerParameters?.ReferencedAssemblies.Cast<string>().ToList();
+        }
+
+        public static void ReferenceIncludes(this MacroEditorForm macroEditorForm, IEnumerable<string> includes)
+        {
+            if (includes.Count() == 0) return;
+
+            var macroEditor = macroEditorForm.GetType().GetField("macroEditor", fieldBindingFlags)?.GetValue(macroEditorForm);
+            var compilerParameters = macroEditor?.GetType().GetField("_compilerParameters", fieldBindingFlags)?.GetValue(macroEditor) as CompilerParameters;
+
+
+            compilerParameters?.ReferencedAssemblies.AddRange(includes.ToArray());
+            macroEditor?.GetType().GetMethod("ResetDotNetProjectResolver", BindingFlags.NonPublic | BindingFlags.InvokeMethod)?.Invoke(macroEditor, new object[] { });
+            macroEditor?.GetType().GetMethod("RemoveRestrictedAssembliesFromCompilerParameters", BindingFlags.NonPublic | BindingFlags.InvokeMethod)?.Invoke(macroEditor, new object[] { });
+        }
+
+        public static void RemoveIncludes(this MacroEditorForm macroEditorForm)
+        {
+            var codeLines = macroEditorForm.MacroCode.Split(new char[] { '\n' }).ToList();
+            codeLines.RemoveAll(line => line.StartsWith("// #include"));
+            macroEditorForm.MacroCode = string.Join("\n", codeLines);
+        }
+
+        public static IEnumerable<string> ParseIncludes(this MacroEditorForm macroEditorForm) => macroEditorForm.MacroCode.Split(new char[] { '\n' }).Where(line => line.StartsWith("// #include")).Select(incl => incl.Substring(incl.IndexOf("\"")).Trim().Trim('\"')).ToList();
+
+        #endregion
+    }
+}
